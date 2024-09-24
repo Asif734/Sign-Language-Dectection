0c3593328f0b2c04be94d82edba79efe4db591d3
Asifuzzaman Asif <asifuzzamanasif734@.com>
2024-09-24 23:16:45 +0600

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Sign-Language-Dectection
+
+## How to run
+
+
+```bash
+    python3 -m venv Sign_env
+```
+```bash
+    source Sign_env/bin/activate
+```
+
+```bash
+    pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'signlanguage',
+    version='0.0.0',
+    author='Asif',
+    author_email='ajasif734@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
